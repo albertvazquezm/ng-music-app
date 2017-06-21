de914957abd594ed0c8819be8a176efda229a39e
Albert Vazquez <albertvazquez@MacBook-Air-de-Albert.local>
2017-06-21 18:36:23 +0200

    update prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
       authenticationUrl: 'https://accounts.spotify.com/authorize',
       clientId: '6cb8cb6350a242a5b913da17bd72eafb',
       responseType: 'token',
-      redirectUri: '/login',
+      redirectUri: '/ng-music-app/login',
       scopes: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-top-read user-read-email user-read-private'
     }
   }
